91089e246943c4321285c29d89ea9afd82d53dca
tora_dotinstall <x.o4o8o.x@gmail.com>
2022-10-20 16:48:18 +0900

    修正しました

--- src/Model.tsx
+++ src/Model.tsx
@@ -14,13 +14,12 @@ import { Plane, Sphere, Stage, useGLTF } from "@react-three/drei";
 
 function Model (url: any) {
     const { scene } = useGLTF(String(url));
-  
     return (
         <Stage>
             <primitive scale={[1, 1, 1]} object={scene} dispose={null} />
         </Stage>
-      )
-  };
-  
+    )
+};
+
 
 export default Model;
